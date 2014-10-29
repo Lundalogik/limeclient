9f6897950b20d32b9c55acd2fa9770936e2ccaa8
jkr <joakim.karlsson@lundalogik.se>
2014-10-29 15:16:56 +0100

    version 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='limeclient',
-    version='0.0.2',
+    version='0.0.3',
     description='Python client for the LIME Pro API',
     classifiers=[
         'Development Status :: 4 - Beta',
