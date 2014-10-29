bb5c2ef5009243e8cbdd250856c297d3078ec961
jkr <joakim.karlsson@lundalogik.se>
2014-10-29 11:33:56 +0100

    Added license and author info.

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@ from setuptools import setup, find_packages
 setup(
     name='limeclient',
     version='0.0.1',
-    packages=find_packages(),
+    description='Python client for the LIME Pro API',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3'
+    ],
+    author='Lundalogik AB',
+    author_email='info@lundalogik.se',
+    license='MPL 2.0',
+
+    packages=['limeclient'],
     include_package_data=True,
 
     install_requires=['requests>=2.4.3']
 )
-
